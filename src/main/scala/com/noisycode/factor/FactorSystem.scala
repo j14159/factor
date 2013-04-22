76300175594eef1db6ddb97a4aa7252f7a10cd04
Jeremy Pierre <j.14159@gmail.com>
2013-04-21 19:59:28 -0700

    Documentation.

--- src/main/scala/com/noisycode/factor/FactorSystem.scala
+++ src/main/scala/com/noisycode/factor/FactorSystem.scala
@@ -2,19 +2,37 @@ package com.noisycode.factor
 
 import scala.annotation.tailrec
 
+/**
+ * Context is provided to actors so that they know their PID (without self()) and
+ * their enclosing system.
+ */
 case class Context[T](pid: FactorPid[T], sys: FactorSystem)
 
+/**
+ * Manages a set of actors and their concurrent execution.
+ */
 class FactorSystem {
+  /**
+   * Shorthand type.
+   */
   type ActorQueue = List[FactorPid[_]]
 
   private var actors: ActorQueue = List()
 
+  /**
+   * Create a new actor.
+   * @param f the initial function to use for processing messages.
+   * @param init the initial actor state.
+   */
   def spawn[T](f: Factor.Factor[T], init: T) = {
     actors = FactorPid(f, init, this) :: actors
     actors.head
   }
     
-
+  /**
+   * Helper method that will let each actor process a single message without
+   * spinning up any threads, etc.
+   */
   def fullRun() {
     actors = fullRun(actors, Nil)
   }
--- src/main/scala/com/noisycode/factor/Factors.scala
+++ src/main/scala/com/noisycode/factor/Factors.scala
@@ -2,17 +2,49 @@ package com.noisycode.factor
 
 import java.util.concurrent.ConcurrentLinkedQueue
 
+/**
+ * Container object for actor function type and actor result
+ * classes.
+ */
 object Factor {
+  /**
+   * Shorthand type for defining partial functions for actors.
+   */
   type Factor[T] = PartialFunction[(Any, Context[T], T), (Result[T])]
 
+  /**
+   * Base trait for all actor results.
+   */
   sealed trait Result[T]
 
+  /**
+   * Return this from an actor to indicate the actor is continuing to operate
+   * without errors.
+   * @param next the next function to use for this actor.
+   * @param state the updated state to use for the next time the actor processes a message.
+   */
   case class Ok[T](next: Factor[T], state: T) extends Result[T]
+  /**
+   * Return this from an actor to reply to whichever actor sent the initial message.
+   * @param reply the message to send back to the calling actor.
+   */
   case class Reply[T](next: Factor[T], reply: Any, state: T) extends Result[T]
+  /**
+   * Indicates a crashed actor.  DERP.
+   */
   case class Crash[T](reason: Throwable, state: T) extends Result[T]
+  /**
+   * Return this to stop the actor in a known/acceptable state (not a crash).
+   */
   case class Stop[T](reason: Any, state: T) extends Result[T]
 }
 
+/**
+ * This class provides a handle to an actor and maintains its mailbox.
+ * @param initFunc the first function to use for message handling in this actor.
+ * @param init the initial state of this actor.
+ * @param sys the [[FactorSystem]] that owns and manages this actor.
+ */
 case class FactorPid[T](initFunc: Factor.Factor[T], init: T, sys: FactorSystem) {
   private[factor] val inbox = new ConcurrentLinkedQueue[(Any, Option[FactorPid[_]])]()
 
@@ -21,8 +53,17 @@ case class FactorPid[T](initFunc: Factor.Factor[T], init: T, sys: FactorSystem) 
   private var state = init
   private var nextFunc = initFunc
 
+  /**
+   * Send (asynchronously) a message to this actor, possibly expecting a reply.
+   * @param msg the message to pass to this actor.
+   * @param sender optionally the actor sending the message.  Supply this if it's being sent by an actor who expects a reply.
+   */
   def ! (msg: Any, sender: Option[FactorPid[_]] = None) = inbox add (msg, sender)
 
+  /**
+   * Causes the actor to receive and process an individual message from it's inbox.
+   * @return true if the actor is still running, false if not.
+   */
   private [factor] def receive() = inbox poll match {
     case null => true
     case (msg, sender) => process(msg, sender) match {
@@ -35,8 +76,14 @@ case class FactorPid[T](initFunc: Factor.Factor[T], init: T, sys: FactorSystem) 
     }
   }
 
+  /**
+   * Convenience method to peek at the actor's state.
+   */
   private [factor] def readState = state
 
+  /**
+   * Handles the actual processing of the message and the result type or crash.
+   */
   private def process(msg: Any, sender: Option[FactorPid[_]]): Option[(Factor[T], T)] = try {
     nextFunc((msg, Context(this, sys), state)) match {
       case Ok(n, s) => Some((n, s))
@@ -53,7 +100,10 @@ case class FactorPid[T](initFunc: Factor.Factor[T], init: T, sys: FactorSystem) 
     case e: Throwable => processCrash(Crash(e, state))
   }
 
-  def processCrash(crash: Crash[T]) = {
+  /**
+   * Deal with the fallout from a crash.
+   */
+  private def processCrash(crash: Crash[T]) = {
     println(s"Stopping pid for crash, ${crash.reason} ::: ${crash.state}")
     //TODO:  handle links
     None
